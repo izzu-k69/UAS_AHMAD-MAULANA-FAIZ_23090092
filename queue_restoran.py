b5386b5738d9a034a2a4ec4e67162144a74164fd
izzu-k69 <izuuu1122@gmail.com>
2024-07-10 12:13:38 +0700

    Add files via upload

---
+++ queue_restoran.py
@@ -0,0 +1,20 @@
+# File: queue_restoran.py
+class QueueRestoran:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+        print(f"Pesanan {item} telah ditambahkan ke dalam antrian.")
+
+    def dequeue(self):
+        if not self.is_empty():
+            item = self.items.pop(0)
+            print(f"Pesanan {item} telah selesai diproses dan dihapus dari antrian.")
+            return item
+        else:
+            print("Antrian kosong.")
+            return None
+
+    def is_empty(self):
+        return len(self.items) == 0
